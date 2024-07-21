e3dee96b63a093dca1a69e9a786ad3e892addd32
Antonio Hickey <antoniohickey99@gmail.com>
2024-07-20 22:53:05 -0400

    feat: add pool tier to `Pool`

--- src/operations/pool.rs
+++ src/operations/pool.rs
@@ -3,9 +3,17 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Deserialize, Debug, Serialize, Clone)]
+pub enum PoolTier {
+    Demo,
+    Hobby,
+    Pro,
+}
+
+#[derive(Deserialize, Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Pool {
     pub pool_id: Uuid,
     pub pool_name: String,
+    pub pool_tier: PoolTier,
     pub creation_date: NaiveDateTime,
 }
